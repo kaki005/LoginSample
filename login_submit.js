5755bf33f81e3e4432072e05e7d710e0062fb9ca
kaki005 <83001142+kaki005@users.noreply.github.com>
2023-01-18 05:55:20 +0900

    Update login_submit.js

--- login_submit.js
+++ login_submit.js
@@ -6,13 +6,10 @@ window.onload = (event) => {
         const password = document.querySelector(".password");
         const username = document.querySelector(".username");
         const result = document.querySelector(".result");
-        console.log(password)
-        console.log(username)
         const url = `https://tvro24petj.execute-api.ap-northeast-1.amazonaws.com/test/?PassWord=${password.value}&Name=${username.value}`;
         const response = await fetch(url, { method: "get" })
         const json = Promise.resolve(response.json()).then(
 	(value) => {
-		console.log(value);
 		result.innerText = value["body"]; 
 	},
 	(value) => {
