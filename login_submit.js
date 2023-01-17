f530b93641812c14c19dcc21b2354a059b920b92
kaki005 <83001142+kaki005@users.noreply.github.com>
2023-01-18 05:44:01 +0900

    index.htmlを追加

---
+++ login_submit.js
@@ -0,0 +1,22 @@
+window.onload = (event) => {
+    const btn = document.querySelector("#submitBtn");
+
+    btn.addEventListener("click", async () => {
+        console.log("aaa")
+        const password = document.querySelector(".password");
+        const username = document.querySelector(".username");
+        const result = document.querySelector(".result");
+        console.log(password)
+        console.log(username)
+        const url = `https://tvro24petj.execute-api.ap-northeast-1.amazonaws.com/test/?PassWord=${password.value}&Name=${username.value}`;
+        const response = await fetch(url, { method: "get" })
+        const json = Promise.resolve(response.json()).then(
+	(value) => {
+		console.log(value);
+		result.innerText = value["body"]; 
+	},
+	(value) => {
+	}
+         );
+    });
+}
