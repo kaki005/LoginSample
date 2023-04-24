2f627937aefab2612a6f5688c03701609e41983d
S Kakio <kakikaki154@DESKTOP-69E6BD8.localdomain>
2023-04-24 09:22:30 +0900

    login_submit.jsの誤りを修正

--- login_submit.js
+++ login_submit.js
@@ -10,13 +10,13 @@ window.onload = (event) => {
         console.log(username)
         const url = `https://tvro24petj.execute-api.ap-northeast-1.amazonaws.com/test?PassWord=${password.value}&UserName=${username.value}`;
         const response = await fetch(url, { method: "get" })
-        const json = Promise.resolve(response.json()).then(
+        const json = Promise.resolve(response.text()).then(
 	        (value) => {
 		        console.log(value);
-		        result.innerText = value["body"]; 
+		        result.innerText = value; 
 	        },
             (value) => {
-                console.log(value);
+                console.error(value);
 	        }
         );
     });
